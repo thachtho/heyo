85a3a99a5389e173102322ca28018bcd2d394c36
thobui14ig <buithanhtho14ig@gmail.com>
2024-07-04 08:37:21 +0700

    ok

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const fs = require('fs');
     const page = await browser.newPage();
 
     // Đi tới URL của trang web
-    await page.goto('https://shopmap.gojo.vn/?phone=+0898123112&language=vi&location=16.9777845,108.2597842&platform=android');
+    await page.goto('https://shopmap.gojo.vn/?phone=+0898456907&language=vi&location=16.9777845,108.2597842&platform=android');
 
     await page.evaluate(scriptContent);
     await page.evaluate(scriptAbc);
